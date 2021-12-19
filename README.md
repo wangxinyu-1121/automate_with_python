8f030d42813244aece9346acab4d9155f3cb764c
王欣宇 <1778743575@qq.com>
2021-12-19 11:37:18 +0800

    项目一：使用多剪切板自动回复消息

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # automate_with_python
 让繁琐工作自动化
 # 项目一：使用多剪贴板自动回复消息（auto_reply_message）
-
+# 项目二：在wiki标记中添加无需列表（add_unordered_list）
---
+++ add_unordered_list/bullet_point_adder.py
@@ -0,0 +1,12 @@
+#! python3
+# bullet_point_adder.py - 在wiki标记中添加无需列表
+"""
+功能：
+1、从剪切板粘贴文本
+2、对它做一些处理
+3、将新的文本复制到剪切板
+"""
+import pyperclip
+text = pyperclip.paste()
+
+
